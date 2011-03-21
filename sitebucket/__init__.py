75c055091dba4aecfc8a95a6920dca340dc80507
thomasw <thomas@matchstrike.net>
2011-03-21 17:38:14 -0400

    Made some objects available directly from the root module and added some metadata.

--- sitebucket/__init__.py
+++ sitebucket/__init__.py
@@ -1,1 +1,11 @@
+'''
+Sitebucket - A threaded site stream monitor for the Twitter API.
+'''
 from listener import SiteStream
+from thread import ListenThread
+from parser import DefaultParser
+from monitor import ListenThreadMonitor
+
+__version__ = '0.0.0'
+__author__ = 'Thomas Welfley'
+__license__ = 'MIT'
